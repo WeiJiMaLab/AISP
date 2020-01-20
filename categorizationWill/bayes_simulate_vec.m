ed29fbc79364ce0228fe4ecdca1170e1edb584fd
Heiko Schuett <heiko.schuett@nyu.edu>
2020-01-20 15:32:40 -0500

    some options chaged

---
+++ categorizationWill/bayes_simulate_vec.m
@@ -0,0 +1,23 @@
+function responses = bayes_simulate_var(data,sigmas,beta,lambda)
+% function responses = bayes_simulate(data,sigmas,beta,lambda)
+% simulates respones of the bayesian observer.
+% data should be a matrix of size trials x 2 with columns:
+%     reliability [1-6] 
+%     stimulus s 
+% sigmas should be a 6 element vector of noise standard deviations
+% beta should be the constant and slope for the logistic link
+% lambda is the lapserate
+
+if ~exist('lambda','var') || isempty(lambda)
+    lambda = 0;
+end
+   
+sigma1 = 3;
+sigma2 = 12;
+sigmaNoise = sigmas(floor(data(:,1)))';
+s = data(:,2);
+x = s + sigmaNoise.*randn(length(s),1);
+d = 1/2*log((sigma2.^2+sigmaNoise.^2)./(sigma1.^2+sigmaNoise.^2))- ...
+    x.^2/2 * (sigma2.^2 - sigma1.^2)./(sigma1.^2+sigmaNoise.^2)./(sigma2^2+sigmaNoise.^2);
+p = lambda/2 + (1-lambda)/(1+exp(beta(1)+beta(2).*d));
+responses = (rand(length(p),1)<p');
--- categorizationWill/fit_cluster_ibs.m
+++ categorizationWill/fit_cluster_ibs.m
@@ -6,24 +6,28 @@ addpath(genpath('../bads/'))
 addpath(genpath('../ibs/'))
 
 options = bads;
+options.NoiseFinalSamples = 100;
 datSubj = data(data(:,1)==iSubj,:);
+opt_ibs = ibslike;
+opt_ibs.Nreps = 25;
+opt_ibs.NegLogLikeThreshold = size(datSubj,1)*log(2)+200;
 %fun_handle = @(pars) likelihood_optim(datSubj,pars,type);
 FUN = @(pars,data) ibs_fun(data,pars,type);
-fun_handle = @(pars) ibslike(FUN,pars,datSubj(:,4),datSubj);
+fun_handle = @(pars) ibslike(FUN,pars,datSubj(:,4),datSubj,opt_ibs);
 switch type
     case 'bayes'
-        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-4,0,0.01],[-3,-3,-3,-3,-3,-3,-10,-2,eps],[6,6,6,6,6,6,10,5,0.25],[-1,-1,-1,-1,-1,-1,-3,-2,0.01],[5,4,3,3,3,2,10,5,0.1]);
+        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-4,0,0.01],[-3,-3,-3,-3,-3,-3,-10,-2,eps],[6,6,6,6,6,6,10,5,0.25],[-1,-1,-1,-1,-1,-1,-3,-2,0.01],[5,4,3,3,3,2,10,5,0.1],options);
         save(sprintf('~/AISP/categorizationWill/pars/pars_ibs_Bayes_%d_%d.mat',iSubj,iRep),'pars','likelihood')
     case 'freq'
-        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1]);
+        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1],options);
         save(sprintf('~/AISP/categorizationWill/pars/pars_ibs_Freq_%d_%d.mat',iSubj,iRep),'pars','likelihood')
     case 'freq2'
-        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1]);
+        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1],options);
         save(sprintf('~/AISP/categorizationWill/pars/pars_ibs_Freq2_%d_%d.mat',iSubj,iRep),'pars','likelihood')
     case 'freq3'
-        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1]);
+        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1],options);
         save(sprintf('~/AISP/categorizationWill/pars/pars_ibs_Freq3_%d_%d.mat',iSubj,iRep),'pars','likelihood')
     case 'var'
-        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1]);
+        [pars,likelihood] = bads(fun_handle,[2,1,0,-1,-1,-1,-2,0,0.01],[-5,-5,-5,-5,-5,-5,-10,-2,eps],[5,5,5,5,5,5,10,5,0.75],[-3,-3,-3,-3,-3,-3,-4,-2,0.01],[5,4,3,3,3,2,10,5,0.1],options);
         save(sprintf('~/AISP/categorizationWill/pars/pars_ibs_Var_%d_%d.mat',iSubj,iRep),'pars','likelihood')
 end
