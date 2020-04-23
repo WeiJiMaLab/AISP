9603ba96f7f62a6f2065a86de70fe406afe93e7d
Heiko Schütt <heiko.schuett@nyu.edu>
2020-04-23 17:48:52 -0400

    cluster scripts updated

--- categorizationWill/test.sh
+++ categorizationWill/test.sh
@@ -7,8 +7,27 @@
 #SBATCH --job-name=myTest
 #SBATCH --mail-type=END
 #SBATCH --mail-user=hhs4@nyu.edu
-#SBATCH --output=slurm-output/slurm_%j.out
+#SBATCH --output=slurm-will/slurm_%j.out
 
 index=$SLURM_ARRAY_TASK_ID
 job=$SLURM_JOB_ID
 ppn=$SLURM_JOB_CPUS_PER_NODE
+
+module purge
+module load matlab/2018b
+export MATLABPATH=$HOME/matlab-output
+
+cat<<EOF | matlab -nodisplay
+job_id = str2num(strjoin(regexp('$job','\d','match'), ''))
+rng(job_id)
+addpath(genpath('$HOME/AISP'))
+
+newdir = '$SCRATCH/cluster$job';
+
+mkdir(newdir);
+cluster_fcn_ibs(job_id,$index);
+
+rmdir(newdir,'s')
+
+
+EOF
