3fc0a321e29ead86538cf078103c9e07226d66c4
jCalderTravis <38797399+jCalderTravis@users.noreply.github.com>
2022-01-17 10:36:36 +0100

    Working on new plot

--- searchJoshua/plotFuns/aisp_plotSimPerformance.m
+++ searchJoshua/plotFuns/aisp_plotSimPerformance.m
@@ -17,15 +17,35 @@ params = {'LnKappa_x', 'LnKappa_x', 'LnKappa_x', 'LnKappa_x', ...
 paramVec = [2.7, 2.6, 2.2, 1.7, 0, 1, 0, 10];
 aisp_checkParamOrder(params, true)
 
+lnKappaRange = -1.6 : 0.2 : 3;
+
 for iM = 1 : length(Config.ModelList)
     modelName = Config.ModelList{iM};
-
-    % Get params into required format
-    ParamStruct = paramVec2Struct(paramVec, modelName, 'toStruct');
-    paramStructs = {ParamStruct};
-    paramStructs = repmapt(paramStructs, 1, length(DSet.P));
+    accAcrossKappa = nan(length(lnKappaRange), 1);
     
-    SimDSet = aisp_simRespAndAcc(DSet, modelName, paramStructs);
+    for iK = 1 : length(lnKappaRange)
+        % Get params into required format
+        if any(strcmp(modelName, {'Bayes', 'PE', 'PE2', 'PE_imagineL'}))
+            thisParamVec = paramVec(1:end-1);
+        else
+            thisParamVec = paramVec;
+        end
+        ParamStruct = paramVec2Struct(thisParamVec, modelName, 'to struct');
+        ParamStruct.LnKappa_x = ParamStruct.LnKappa_x + lnKappaRange(iK);
+        paramStructs = {ParamStruct};
+        paramStructs = repmat(paramStructs, 1, length(DSet.P));
+        
+        nReps = 10;
+        allAcc = nan(nReps, 1);
+        for iRep = 1 : nReps
+            SimDSet = aisp_simRespAndAcc(DSet, modelName, paramStructs);
+            acc = mT_stackData(SimDSet.P, @(str)mean(str.Data.Accuracy));
+            allAcc(iRep) = mean(acc);
+        end
+        
+        accAcrossKappa(iK) = mean(allAcc);
+        disp(['Finished kappa iter ', num2str(iK)])
+    end
 end
 
 % THIS FUN IS WORK IN PROGRESS
