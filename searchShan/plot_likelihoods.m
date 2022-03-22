f0c490f538c264265c864d31ed0a8cd5d5e95f0e
Heiko Schuett <heiko.schuett@nyu.edu>
2022-03-22 16:45:37 -0400

    update searchShan plots

--- searchShan/plot_likelihoods.m
+++ searchShan/plot_likelihoods.m
@@ -1,4 +1,8 @@
-function plot_likelihoods(Naverage)
+function plot_likelihoods(Naverage, subset)
+
+if ~exist('subset','var') || isempty(subset)
+    subset = 1:4;
+end
 
 Nsubs = 9;
 
@@ -18,7 +22,7 @@ for itype = 1:4
     f = load(fname);
     if itype>5
         sorted = sort(f.likelihoods,2);
-        if exist('Naverage','var')
+        if exist('Naverage','var') && ~isempty(Naverage)
             stds(:,itype) = std(sorted(:,1:Naverage),[],2,'omitnan')/log(2);
             likelihoods(:,itype) = mean(sorted(:,1:Naverage),2,'omitnan')/log(2);
         else
@@ -27,7 +31,7 @@ for itype = 1:4
         end
     else
         sorted = sort(f.likelihoods,2);
-        if exist('Naverage','var')
+        if exist('Naverage','var') && ~isempty(Naverage)
             stds(:,itype) = std(sorted(:,1:Naverage),[],2,'omitnan');
             likelihoods(:,itype) = mean(sorted(:,1:Naverage),2,'omitnan');
         else
@@ -40,14 +44,16 @@ end
 pdata = likelihoods(:,1)-likelihoods;
 
 figure 
-bar(mean(pdata),'FaceColor','k')
+bar(mean(pdata(:, subset)),'FaceColor','k')
 hold on
-plot((1:4) +0.05*randn(size(pdata)),pdata,'.','Color',[0.4,0.4,0.4],'MarkerSize',20)
+plot((1:length(subset)) +0.05*randn(size(pdata(:,subset))),pdata(:,subset),'.','Color',[0.4,0.4,0.4],'MarkerSize',20)
 box off
 set(gca,'TickDir','out', 'LineWidth', 2)
 ylabel('LL - LLBayes','FontSize',20)
 set(gca,'FontSize', 18, 'XColor', 'k', 'YColor', 'k')
-xticklabels({'Bayes', 'Point Estimate', 'Sampling', 'joint Sampling'})
+labels = {'Bayes', 'Point Estimate', 'Sampling', 'joint Sampling'};
+xticklabels(labels(subset));
+xlim([0.5, length(subset) + 0.5])
 set(gcf, 'Position', [550   750   560   250])
 
 
@@ -56,7 +62,7 @@ pdata2 = stds;
 figure 
 bar(mean(pdata2),'FaceColor','k')
 hold on
-plot((1:4) +0.05*randn(size(pdata2)),pdata2,'.','Color',[0.4,0.4,0.4],'MarkerSize',20)
+plot((1:length(subset)) +0.05*randn(size(pdata2(:,subset))),pdata2(:,subset),'.','Color',[0.4,0.4,0.4],'MarkerSize',20)
 box off
 set(gca,'TickDir','out')
 ylabel('std of estimates','FontSize',20)
